77ddb88f2713ba3afdd16497cc61c0b758ef78d5
Edeilton Oliveira <edeiltonso@gmail.com>
2023-09-29 02:43:08 -0300

    feat: elimina prefetch dos produtos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,9 +32,8 @@ export default function Home({products}: HomeProps) {
       
       {products.map(product => {
         return (
-          <Link key={product.id} href={`/product/${product.id}`}> 
-            <Product className="keen-slider__slide"
-            >
+          <Link key={product.id} href={`/product/${product.id}`} prefetch={false}> 
+            <Product className="keen-slider__slide">
               <Image src={product.imageUrl} width={520} height={480} alt='' />
               
               <footer>
